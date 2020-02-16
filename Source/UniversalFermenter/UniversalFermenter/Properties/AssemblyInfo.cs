e3ecbba677ddc55ac74c33a41f51554d2287bc03
Taranchuk <nfhfyxer@gmail.com>
2020-02-16 10:39:13 +0200

    Altered AssemblyInfo
    When there are other mods with UniversalFermeter dll, then dlls are replaced, I made dll unique to avoid conflicts

--- Source/UniversalFermenter/UniversalFermenter/Properties/AssemblyInfo.cs
+++ Source/UniversalFermenter/UniversalFermenter/Properties/AssemblyInfo.cs
@@ -6,18 +6,18 @@ using System.Runtime.InteropServices;
 using System.Security;
 using System.Security.Permissions;
 
-[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.1.0.1")]
 [assembly: CompilationRelaxations(8)]
 [assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
 [assembly: Debuggable(DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
-[assembly: AssemblyTitle("UniversalFermenter")]
+[assembly: AssemblyTitle("MOP_Fermenter")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("UniversalFermenter")]
+[assembly: AssemblyProduct("MOP_Fermenter")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("5372ede0-034e-43bd-9f46-024bd00055b9")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.1")]
 [assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
