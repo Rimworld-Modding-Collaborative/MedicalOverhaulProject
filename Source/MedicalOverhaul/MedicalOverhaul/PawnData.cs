4f4159b98b1ff4b3d1b94713658be4a41e3c211a
Taranchuk <nfhfyxer@gmail.com>
2020-02-18 06:57:21 +0200

    Disease tracker

---
+++ Source/MedicalOverhaul/MedicalOverhaul/PawnData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Verse;
+
+namespace MedicalOverhaul
+{
+    public class PawnData : IExposable
+    {
+        public PawnData()
+        {
+        }
+
+        public PawnData(int totalChronicDiseases)
+        {
+            this.totalChronicDiseases = totalChronicDiseases;
+        }
+
+        public void ExposeData()
+        {
+            Scribe_Values.Look<int>(ref this.totalChronicDiseases, "totalChronicDiseases", 0, true);
+        }
+
+        public int totalChronicDiseases;
+
+    }
+}
